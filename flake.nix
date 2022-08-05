b9e63e310d3a052b7b438c7da7a401f6c4e836a7
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-08-04 21:53:06 -0700

    Fix yarn pkg

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [ pkgs.nodejs-18_x pkgs.nodePackages_latest.yarn ];
+        buildInputs = [ pkgs.nodejs-18_x pkgs.yarn ];
       };
     };
 }
