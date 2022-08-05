9999eca68da93941e0f6db8fb2466725d65eca80
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-08-05 10:30:46 -0700

    apollo-server-fastify init

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,5 @@
+import { ApolloServer } from "apollo-server-fastify";
+import fastify from "fastify";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { AppDataSource } from "./data-source";
@@ -8,7 +10,17 @@ async function main() {
   const schema = await buildSchema({
     resolvers: [HelloResolver],
   });
-  console.log(schema);
+  const app = fastify();
+  const server = new ApolloServer({
+    schema,
+  });
+  await server.start();
+  app.register(server.createHandler());
+
+  const host = "127.0.0.1";
+  const port = 8080;
+  await app.listen({ host, port });
+  console.log(`Listening on http://${host}:${port}`);
 }
 
 main();
