467fca1f89683a40146b46a2f5b6a988a4eb9510
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-08-07 18:26:50 -0700

    Apollo client setup

---
+++ frontend/src/gqlClient.ts
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const backend = process.env.NEXT_PUBLIC_BACKEND_URL;
+if (!backend) throw new Error("NEXT_PUBLIC_BACKEND_URL is unset");
+
+export const client = new ApolloClient({
+  uri: backend + "/graphql",
+  cache: new InMemoryCache(),
+});
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import { ApolloProvider } from "@apollo/client";
 import { AppProps } from "next/app";
+import { client } from "../gqlClient";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
