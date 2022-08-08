28d4ded44843096f4310b518ea718868687bbb26
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-08-07 18:02:58 -0700

    minimal homepage

---
+++ frontend/src/components/LinkButton.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { PropsWithChildren } from "react";
+
+export type LinkButtonProps = { href: string; primary?: bool };
+
+export const LinkButton = ({
+  children,
+  href,
+  primary = true,
+}: PropsWithChildren<LinkButtonProps>): JSX.Element => {
+  return (
+    <Link
+      href={href}
+      className={
+        "mx-2 rounded-md px-6 py-2.5 " +
+        (primary ? "bg-blue-500" : "outline outline-1 outline-blue-500")
+      }
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default LinkButton;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,19 @@
+import LinkButton from "../components/LinkButton";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello, world!</h1>;
+  return (
+    <div className="h-full flex justify-center items-center">
+      <div className="">
+        <div className="w-full flex justify-center mb-5">
+          <h1 className="text-5xl font-bold inline">Stronger</h1>
+        </div>
+        <div className="w-full flex justify-center">
+          <LinkButton href="#" primary={false}>
+            Login
+          </LinkButton>
+          <LinkButton href="#">Register</LinkButton>
+        </div>
+      </div>
+    </div>
+  );
 }
