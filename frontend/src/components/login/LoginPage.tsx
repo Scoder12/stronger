b2505da1f5f89868a1c5f3d387ed0714f29ff61f
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-09-22 17:20:43 -0700

    LoginPage refactor

--- frontend/src/components/login/LoginPage.tsx
+++ frontend/src/components/login/LoginPage.tsx
@@ -1,10 +1,5 @@
-import { FetchResult } from "@apollo/client";
 import { Field, FieldProps, Form, Formik, useField } from "formik";
-import {
-  LoginMutation,
-  LoginMutationVariables,
-  useLoginMutation,
-} from "src/generated/graphql";
+import { useLoginMutation } from "src/generated/graphql";
 import Button from "../Button";
 import Center from "../Center";
 
@@ -37,70 +32,63 @@ const Input = ({
   );
 };
 
-export interface LoginFormProps {
-  loading: boolean;
-  login: (
-    vars: LoginMutationVariables
-  ) => Promise<
-    FetchResult<LoginMutation, Record<string, any>, Record<string, any>>
-  >;
-}
-
 function fieldErrorsToFormik(errors: { field: string; message: string }[]) {
   return Object.fromEntries(errors.map((e) => [e.field, e.message]));
 }
 
-export const LoginForm = ({ loading, login }: LoginFormProps): JSX.Element => {
+export const LoginForm = (): JSX.Element => {
+  const [login, { data, loading, error }] = useLoginMutation();
+
+  if (data && data.login.user) return <p>{"hi " + data.login.user}</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
-    <Center className="h-full">
-      <div className="w-full max-w-sm px-3">
-        <h1 className="text-4xl text-bold">Login</h1>
-        <Formik
-          initialValues={{ username: "", password: "" }}
-          onSubmit={async (
-            values: { username: string; password: string },
-            { setErrors }
-          ) => {
-            const res = await login(values);
-            const fieldErrors = res.data?.login.errors;
-            if (fieldErrors) {
-              const e = fieldErrorsToFormik(fieldErrors);
-              setErrors(e);
-            }
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <Field name="username" label="Username" component={Input} />
-              <Field
-                name="password"
-                label="Password"
-                component={Input}
-                type="password"
-              />
-              <Button
-                type="submit"
-                className="float-right mt-2"
-                disabled={isSubmitting}
-                loading={loading}
-              >
-                Login
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      </div>
-    </Center>
+    <>
+      <h1 className="text-4xl text-bold">Login</h1>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (
+          values: { username: string; password: string },
+          { setErrors }
+        ) => {
+          const res = await login({ variables: values });
+          const fieldErrors = res.data?.login.errors;
+          if (fieldErrors) {
+            const e = fieldErrorsToFormik(fieldErrors);
+            setErrors(e);
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field name="username" label="Username" component={Input} />
+            <Field
+              name="password"
+              label="Password"
+              component={Input}
+              type="password"
+            />
+            <Button
+              type="submit"
+              className="float-right mt-2"
+              disabled={isSubmitting}
+              loading={loading}
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
 
 export const LoginPage = (): JSX.Element => {
-  const [login, { data, loading, error }] = useLoginMutation();
-
-  if (data && data.login.user) return <p>{"hi " + data.login.user}</p>;
-  if (error) return <p>oopsies . </p>;
-
   return (
-    <LoginForm loading={loading} login={(variables) => login({ variables })} />
+    <Center className="h-full">
+      <div className="w-full max-w-sm px-3">
+        <LoginForm />
+      </div>
+    </Center>
   );
 };
